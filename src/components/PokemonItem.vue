b7219b5437a5ed598a1f18c82bb134f34746fdd7
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-08-29 12:43:29 +0200

    Add base-dialog component

--- src/components/PokemonItem.vue
+++ src/components/PokemonItem.vue
@@ -4,10 +4,33 @@
       <img :src="props.img" :alt="props.name" />
       <h2>{{ props.name }}</h2>
       <BaseBadge :type="type" :title="type" />
+      <BaseDialog @close="hideDialog" :open="dialogIsVisible">
+        <img :src="props.img" :alt="props.name" />
+        {{ props.name }}
+        {{ props.type }}
+        {{ props.attacks }}
+        <button @click="hideDialog">Close it!</button>
+      </BaseDialog>
+      <br />
+      <div>
+        <button @click="showDialog">Show Dialog</button>
+      </div>
     </BaseCard>
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
+
 const props = defineProps(["id", "img", "name", "type", "attacks"]);
+
+let dialogIsVisible = ref(false);
+
+function showDialog() {
+  dialogIsVisible.value = true;
+}
+
+function hideDialog() {
+  dialogIsVisible.value = false;
+}
 </script>
