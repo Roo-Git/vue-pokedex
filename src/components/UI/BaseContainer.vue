9e34121014cfdb5e0d1cf38a3775c6716e1b104e
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-08-30 18:18:16 +0200

    style

---
+++ src/components/UI/BaseContainer.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="grid grid-cols-2 grid-rows-2 gap-1 w-100 bg-stone-100 rounded-3xl m-2 p-6"
+  >
+    <slot></slot>
+  </div>
+</template>
+
+<script></script>
+
+<style scoped></style>
