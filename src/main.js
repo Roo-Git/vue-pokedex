312a9c609c5ca490975edcbc825bebf74fdf0c90
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-08-30 18:16:29 +0200

    import global components

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from "./App.vue";
 import BaseBadge from "./components/UI/BaseBadge.vue";
 import BaseCard from "./components/UI/BaseCard.vue";
 import BaseDialog from "./components/UI/BaseDialog.vue";
+import BaseContainer from "./components/UI/BaseContainer.vue";
+import BaseButton from "./components/UI/BaseButton.vue";
+
 import "./assets/tailwind.css";
 
 const app = createApp(App);
@@ -10,5 +13,7 @@ const app = createApp(App);
 app.component("BaseBadge", BaseBadge);
 app.component("BaseCard", BaseCard);
 app.component("BaseDialog", BaseDialog);
+app.component("BaseContainer", BaseContainer);
+app.component("BaseButton", BaseButton);
 
 app.mount("#app");
