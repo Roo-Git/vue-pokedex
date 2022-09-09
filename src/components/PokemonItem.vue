1704f1fd33ac86438ffd1b13b90e26ada6e3396d
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-09-09 17:48:00 +0200

    add animation to card

--- src/components/PokemonItem.vue
+++ src/components/PokemonItem.vue
@@ -4,27 +4,27 @@
       <img :src="props.img" :alt="props.name" />
       <h2 class="pokemon-card-name">{{ props.name }}</h2>
       <BaseBadge :type="type" :title="type" />
-      <BaseDialog @close="hideDialog" :type="type" :open="dialogIsVisible">
-        <div class="dialog-card-container">
-          <h2 class="pokemon-dialog-name">
-            {{ props.name }}
-          </h2>
-          <img :src="props.dreamImg" :alt="props.name" />
-          <p class="pokemon-data">Type: {{ props.type.toUpperCase() }}</p>
-          <p class="pokemon-data">Height: {{ props.height }}</p>
-          <p class="pokemon-data">Weight: {{ props.weight }}</p>
-          <p class="pokemon-data">Attack: {{ props.attacks }}</p>
-          <BaseButton
-            @click="hideDialog"
-            class="close-dialog-btn"
-            :title="closeBtn"
-          />
-        </div>
-      </BaseDialog>
       <div>
         <BaseButton :type="type" :title="info" @click="showDialog" />
       </div>
     </BaseCard>
+    <BaseDialog @close="hideDialog" :type="type" :open="dialogIsVisible">
+      <div class="dialog-card-container">
+        <h2 class="pokemon-dialog-name">
+          {{ props.name }}
+        </h2>
+        <img :src="props.dreamImg" :alt="props.name" />
+        <p class="pokemon-data">Type: {{ props.type.toUpperCase() }}</p>
+        <p class="pokemon-data">Height: {{ props.height }}</p>
+        <p class="pokemon-data">Weight: {{ props.weight }}</p>
+        <p class="pokemon-data">Attack: {{ props.attacks }}</p>
+        <BaseButton
+          @click="hideDialog"
+          class="close-dialog-btn"
+          :title="closeBtn"
+        />
+      </div>
+    </BaseDialog>
   </div>
 </template>
 
