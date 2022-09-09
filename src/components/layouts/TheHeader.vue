d732861641f04e6aa89bd6f57285d5f49cdfffdf
Roo-Git <robert.martinez.delafuente@gmail.com>
2022-09-09 13:34:15 +0200

    add styles to tailwind.css

--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -1,15 +1,11 @@
 <template>
   <div>
-    <BaseWrapper class="flex justify-around bg-stone-200 rounded-3xl m-2 p-2">
-      <h1
-        class="flex items-center text-red-600 text-3xl font-bold tracking-wide"
-      >
-        POKEDEX APP
-      </h1>
+    <BaseWrapper class="wrapped-header">
+      <h1 class="app-title">POKEDEX APP</h1>
       <img
         src="../../assets/img/pokeball.svg"
         alt="pokeball"
-        class="flex items-center w-16"
+        class="pokeball-img"
       />
     </BaseWrapper>
   </div>
